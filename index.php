df658cf9f994db2503ae507503aeca39ab07d02b
ze <ze@192.168.3.2>
2013-08-19 08:00:05 +0100

    WordPress style coding conventions

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-/** 
+/**
 
-phpSAFE - PHP Security Analysis For Everyone
-	by Jose Fonseca (jozefonseca@gmail.com)
-			
+  phpSAFE - PHP Security Analysis For Everyone
+  by Jose Fonseca (jozefonseca@gmail.com)
 
-Copyright (C) 2013 Jose Fonseca
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+  Copyright (C) 2013 Jose Fonseca
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.		
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-**/
+  You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+ */
 require_once 'class-php-safe.php';
 
 echo "<!DOCTYPE html><title>" . APP . "</title><html><head>";
